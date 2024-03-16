705bf95f750b84afeafe9b48577d898820f36722
webzille <153772404+webzille@users.noreply.github.com>
2024-03-16 00:30:59 -0500

    Update test.php

--- test.php
+++ test.php
@@ -4,45 +4,62 @@ require 'vendor/autoload.php';
 
 use Webzille\ColorUtility\Colors\HEX;
 use Webzille\ColorUtility\Colors\LAB;
+use Webzille\ColorUtility\Colors\RGBA;
+use Webzille\ColorUtility\SetColor;
 
 echo "<pre>";
 
-$color1 = new LAB(50, -52, -25);
-$color2 = new LAB(50, -52, 25);
-$color3 = new LAB(50, 52, 25);
-$color4 = new LAB(50, 52, -25);
-$color4AsHEX = $color4->asHEX();
+$HexString = "#ffcc00";
+$HexObject = SetColor::fromString($HexString);
+echo $HexObject->viewColor("Hex Color from String");
 
-echo PHP_EOL . $color4AsHEX->viewColor("Monochromatic Shades for");
-foreach($color4AsHEX->monochromaticShadeColors() as $shadeHEX) {
-    echo $shadeHEX->viewColor();
+$HexString2 = "aa77dd";
+$HexObject2 = SetColor::fromString($HexString2);
+echo $HexObject2->viewColor();
+
+$RGBString = "rgb(255, 0, 0);";
+$RGBObject = SetColor::fromString($RGBString);
+echo $RGBObject->viewColor();
+
+$RGBAString = "rgba(153, 255, 46, 0.4)";
+$RGBAObject = SetColor::fromString($RGBAString);
+echo $RGBAObject->viewColor();
+
+$HSLAString = "hsla(0, 41%, 51%, 0.3)";
+$HSLAObject = SetColor::fromString($HSLAString);
+echo $HSLAObject->viewColor();
+
+$HSLString = "hsl(146, 41%, 51%)";
+$HSLObject = SetColor::fromString($HSLString);
+echo $HSLObject->viewColor();
+
+$NamedString = "steelblue";
+$NamedObject = SetColor::fromString($NamedString);
+echo $NamedObject->viewColor();
+
+$blackString = 'black';
+$blackObject = SetColor::fromString($blackString);
+echo $blackObject->asLAB()->viewColor("Converted from a non-websafe color format");
+
+echo $RGBAObject . PHP_EOL . PHP_EOL;
+
+$tetradicColors = $HexObject2->asHSL()->tetradicColors();
+echo $HexObject2->viewColor("Tetradic Colors of");
+foreach ($tetradicColors as $tetradicColor) {
+    echo $tetradicColor->viewColor();
 }
-
-echo PHP_EOL . $color2->viewColor("Monochromatic Shades for");
-foreach($color2->monochromaticShadeColors() as $shade) {
-    echo $shade->viewColor("RGB (". $shade->asRGB() .")");
+echo PHP_EOL;
+$tetradicColors2 = $HexObject2->asLAB()->tetradicColors();
+echo $HexObject2->viewColor("Tetradic Color in LAB (LAB converts to RGB since it isn't websafe)");
+foreach ($tetradicColors2 as $tetradicColor2) {
+    echo $tetradicColor2->viewColor();
 }
+echo PHP_EOL;
+$hsla = $HexObject2->asHSLA();
+echo $HexObject2->viewColor("For Reference");
+echo $hsla->viewColor("The color");
 
-$distance = $color1->visibleDifference($color2);
-echo PHP_EOL . $distance . PHP_EOL . PHP_EOL;
-echo $color4->viewColor("Reference Color");
-$linearColor = $color4->linearDeviance($distance);
-echo $linearColor->viewColor("Linear Color");
-echo $linearColor->visibleDifference($color4) . PHP_EOL;
-$angularColor = $color4->angularDeviance($distance);
-echo $angularColor->viewColor("Angular Color");
-echo $angularColor->visibleDifference($color4) . PHP_EOL;
-echo $color4->findColorByAngle($distance)->viewColor("'$distance' Degrees");
-$ref = $color4;
-$difference = 20;
-echo $ref->viewColor("The reference color");
-$distance = $ref->findColorAtDifference($difference);
-echo $distance->viewColor("Distance Color");
-echo $distance->visibleDifference($ref) . PHP_EOL;
-echo "Expected distance is: $difference\n";
-
-$hex = new HEX('fff');
-echo $hex->viewColor('White HEX');
-echo $hex->asLAB() . PHP_EOL;
-$cylindrical = $hex->asCylindrical();
-echo $cylindrical . PHP_EOL;
+$alphaRGBA = new RGBA(234, 21, 148, 0.5);
+echo $alphaRGBA->viewColor("Transparency Here");
+echo $alphaRGBA->asHSLA()->viewColor("As HSLA");
+echo $alphaRGBA->asHEX()->viewColor("As HEX");
