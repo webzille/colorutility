527f35efdfe18af9db3395afd433a3a0449d6f9b
webzille <153772404+webzille@users.noreply.github.com>
2024-03-25 18:53:52 -0500

    Update Color.php

--- src/Color.php
+++ src/Color.php
@@ -55,7 +55,7 @@ abstract class Color {
         return $this;
     }
 
-    public function asCylindricalLAB(): CylindricalLAB
+    public function asCylindrical(): CylindricalLAB
     {
         return $this;
     }
