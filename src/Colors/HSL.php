d2fd0ad8a2b0e2bf4e63e34c9c10c831348e8216
webzille <153772404+webzille@users.noreply.github.com>
2024-04-10 18:29:21 -0500

    Update HSL.php

--- src/Colors/HSL.php
+++ src/Colors/HSL.php
@@ -64,15 +64,12 @@ class HSL extends Color
 
     public function findColorByAngle(float $angle): self
     {
-        $newHue = ($this->h + $angle) % 360;
+        return $this->asRGB()->findColorByAngle($angle)->asHSL();
+        $angle = fmod(($angle + 360), 360);
 
-        if ($newHue < 0) {
-            $newHue += 360;
-        }
+        $newHue = fmod(($this->h + $angle + 360), 360);
 
-        $traditionalHue = $newHue;
-
-        return new self($traditionalHue, $this->s, $this->l);
+        return new HSL($newHue, $this->s, $this->l);
     }
 
     public function findColorAtDifference(float $difference): self
@@ -137,52 +134,40 @@ class HSL extends Color
 
     public function asRGB(): RGB
     {
-        $hue = $this->h / 360;
-        $saturation = $this->s / 100;
-        $lightness = $this->l  / 100;
+        $h = $this->h / 360;
+        $s = $this->s / 100;
+        $l = $this->l / 100;
 
-        $chroma = (1 - abs(2 * $lightness - 1)) * $saturation;
-        $hueSector = $hue * 6;
-        $x = $chroma * (1 - abs((int) $hueSector % 2 - 1));
+        if ($s === 0) {
+            $r = $l;
+            $g = $l;
+            $b = $l;
+        } else {
+            $q = ($l < 0.5) ? $l * (1 + $s) : $l + $s - $l * $s;
+            $p = 2 * $l - $q;
 
-        $r1 = $g1 = $b1 = 0;
-        switch (floor($hueSector)) {
-            case 0:
-                $r1 = $chroma;
-                $g1 = $x;
-                break;
-            case 1:
-                $r1 = $x;
-                $g1 = $chroma;
-                break;
-            case 2:
-                $g1 = $chroma;
-                $b1 = $x;
-                break;
-            case 3:
-                $g1 = $x;
-                $b1 = $chroma;
-                break;
-            case 4:
-                $r1 = $x;
-                $b1 = $chroma;
-                break;
-            case 5:
-                $r1 = $chroma;
-                $b1 = $x;
-                break;
+            $r = $this->hueToRGB($p, $q, $h + 1/3);
+            $g = $this->hueToRGB($p, $q, $h);
+            $b = $this->hueToRGB($p, $q, $h - 1/3);
         }
 
-        $m = $lightness - $chroma / 2;
-        $r = ($r1 + $m) * 255;
-        $g = ($g1 + $m) * 255;
-        $b = ($b1 + $m) * 255;
-
-        $r = max(0, min(255, $r));
-        $g = max(0, min(255, $g));
-        $b = max(0, min(255, $b));
+        $r = round($r * 255);
+        $g = round($g * 255);
+        $b = round($b * 255);
 
         return new RGB($r, $g, $b);
+    }
+
+    private function hueToRGB($p, $q, $t)
+    {
+        if ($t < 0) $t += 1;
+        if ($t > 1) $t -= 1;
+
+        if ($t < 1/6) return $p + ($q - $p) * 6 * $t;
+        if ($t < 1/2) return $q;
+        if ($t < 2/3) return $p + ($q - $p) * (2/3 - $t) * 6;
+
+        return $p;
     }
 
     public function asHSLA(float $alpha = 1): HSLA
