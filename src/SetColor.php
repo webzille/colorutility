577b0c853649737097a1b9586978b58f27d5e2c9
webzille <153772404+webzille@users.noreply.github.com>
2024-04-22 20:29:59 -0500

    Update SetColor.php

--- src/SetColor.php
+++ src/SetColor.php
@@ -4,26 +4,15 @@ namespace Webzille\ColorUtility;
 
 use Webzille\ColorUtility\Colors\RGB;
 use Webzille\ColorUtility\Colors\HEX;
-use Webzille\ColorUtility\Colors\HSL;
-use Webzille\ColorUtility\Colors\HSLA;
 use Webzille\ColorUtility\Colors\RGBA;
 
 class SetColor {
-
-    private static array $colors = [
-        'rgba'  => RGBA::class,
-        'rgb'   => RGB::class,
-        'hsla'  => HSLA::class,
-        'hsl'   => HSL::class,
-    ];
-
-    private static string $namedColors = "NamedColorsLookupTable.php";
 
     public static function fromString(string $colorString)
     {
         $colorString = strtolower($colorString);
 
-        foreach (self::$colors as $color => $colorClass) {
+        foreach (Colors::$settable as $color => $colorClass) {
             if (strpos($colorString, strtolower($color)) !== false) {
                 $colorValues = self::getColors($colorString);
                 
@@ -31,16 +20,14 @@ class SetColor {
             }
         }
 
-        if (preg_match('/^(#)?([0-9a-fA-F]{2,4}|[0-9a-fA-F]{6,8})$/', $colorString, $matches)) {
+        if (preg_match(Colors::$hexPattern, $colorString, $matches)) {
             $hexColor = isset($matches[2]) ? $matches[2] : $matches[0];
             
             return new HEX($hexColor);
         }
 
-        $namedColors = require self::$namedColors;
-
-        if (isset($namedColors[$colorString])) {
-            $colorValue = explode(',', $namedColors[$colorString]['rgb'] ?? $namedColors[$colorString]['rgba']);
+        if (isset(Colors::$namedColors[$colorString])) {
+            $colorValue = explode(',', Colors::$namedColors[$colorString]['rgb'] ?? Colors::$namedColors[$colorString]['rgba']);
 
             $colorClass = count($colorValue) === 3 ? RGB::class : RGBA::class;
             
@@ -51,7 +38,7 @@ class SetColor {
     }
 
     private static function getColors($colorString) {
-        preg_match_all('/(?:\.\d+|\d+(?:\.\d+)?)/', $colorString, $matches);
+        preg_match_all(Colors::$valuePattern, $colorString, $matches);
         
         return $matches[0];
     }
